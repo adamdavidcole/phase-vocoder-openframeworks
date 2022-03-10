aea4a1698ab610043412dac346468e8bc60f1476
Adam Cole <acole9@gmail.com>
2022-03-10 22:38:06 +0000

    overlapp-add beginning

---
+++ src/CircularBuffer.cpp
@@ -0,0 +1,8 @@
+//
+//  CircularBuffer.cpp
+//  sing-to-me
+//
+//  Created by Adam Cole on 3/10/22.
+//
+
+#include "CircularBuffer.hpp"
---
+++ src/CircularBuffer.hpp
@@ -0,0 +1,13 @@
+//
+//  CircularBuffer.hpp
+//  sing-to-me
+//
+//  Created by Adam Cole on 3/10/22.
+//
+
+#ifndef CircularBuffer_hpp
+#define CircularBuffer_hpp
+
+#include <stdio.h>
+
+#endif /* CircularBuffer_hpp */
