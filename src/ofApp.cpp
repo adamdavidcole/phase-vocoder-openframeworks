5c7e37fc6b42e676ed111b66b04797d5405350b1
Adam Cole <acole9@gmail.com>
2022-03-25 20:55:30 +0000

    phase one and two...good enough

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,7 +1,7 @@
 #include "ofApp.h"
 
 #define DISABLE_AUDIO false
-#define DISABLE_VIDEO true
+#define DISABLE_VIDEO false
 
 float mtofArray[] = {0, 8.661957, 9.177024, 9.722718, 10.3, 10.913383, 11.562325, 12.25, 12.978271, 13.75, 14.567617, 15.433853, 16.351599, 17.323914, 18.354048, 19.445436, 20.601723, 21.826765, 23.124651, 24.5, 25.956543, 27.5, 29.135235, 30.867706, 32.703197, 34.647827, 36.708096, 38.890873, 41.203445, 43.65353, 46.249302, 49., 51.913086, 55., 58.27047, 61.735413, 65.406395, 69.295654, 73.416191, 77.781746, 82.406891, 87.30706, 92.498604, 97.998856, 103.826172, 110., 116.540939, 123.470825, 130.81279, 138.591309, 146.832382, 155.563492, 164.813782, 174.61412, 184.997208, 195.997711, 207.652344, 220., 233.081879, 246.94165, 261.62558, 277.182617,293.664764, 311.126984, 329.627563, 349.228241, 369.994415, 391.995422, 415.304688, 440., 466.163757, 493.883301, 523.25116, 554.365234, 587.329529, 622.253967, 659.255127, 698.456482, 739.988831, 783.990845, 830.609375, 880., 932.327515, 987.766602, 1046.502319, 1108.730469, 1174.659058, 1244.507935, 1318.510254, 1396.912964, 1479.977661, 1567.981689, 1661.21875, 1760., 1864.655029, 1975.533203, 2093.004639, 2217.460938, 2349.318115, 2489.015869, 2637.020508, 2793.825928, 2959.955322, 3135.963379, 3322.4375, 3520., 3729.31, 3951.066406, 4186.009277, 4434.921875, 4698.63623, 4978.031738, 5274.041016, 5587.651855, 5919.910645, 6271.926758, 6644.875, 7040., 7458.620117, 7902.132812, 8372.018555, 8869.84375, 9397.272461, 9956.063477, 10548.082031, 11175.303711, 11839.821289, 12543.853516, 13289.75};
 
@@ -181,13 +181,28 @@ void ofApp::update() {
         {
             float phaseTwoStart = 5;
             float phaseTwoDuration = 10;
-            float phaseTwoMaxDistortion = 0.5;
+            float phaseTwoMaxDistortion = 0.4;
             float progress = ofMap(ellapsedTime, phaseTwoStart, phaseTwoStart + phaseTwoDuration, 0, 1);
 
-            float phase = ofMap(sin(ellapsedTime), -1, 1, 0, phaseTwoMaxDistortion) * progress;
+            float phase = ofMap(sin(ellapsedTime* 5.0 * progress), -1, 1, 0, phaseTwoMaxDistortion) * (progress + 0.25);
 
-//            glitchAmount = phase;
-//            glitchIntensity = phase; //mapSquared(glitchAmount, 0, phaseTwoMaxDistortion, 0, phaseTwoMaxDistortion);
+//            float glitchAmountStartIncrease ofMap()
+            glitchAmount = phase;
+            glitchIntensity = phase;
+            
+            phaseVocoder.glitchAmount = glitchAmount;
+            phaseVocoder.glitchIntensity = glitchIntensity;
+            
+            float pitchShiftDegree = floor(ofMap(glitchAmount, 0, phaseTwoMaxDistortion, 0, 12));
+            if (ofRandom(1.0) > 0.5) pitchShiftDegree*= -1;
+            float pitchShfit = powf(2.0, (int)pitchShiftDegree / 12.0);
+    //
+            phaseVocoder.setMode(simplePitchShift);
+            phaseVocoder.setPitchShift(pitchShfit);
+            
+//            setPhaseVocoderMode();
+
+            //mapSquared(glitchAmount, 0, phaseTwoMaxDistortion, 0, phaseTwoMaxDistortion);
         }
             break;
 //        case AppState::RUNNING_PHASE_THREE:
@@ -246,8 +261,7 @@ void ofApp::update() {
 //    float phase = sin(ellapsedTime) * ofMap(ellapsedTime, 0, 60, 0, 1);
 //    glitchAmount = ofMap(phase, -1, 1, 0, 2);
 //    glitchIntensity = mapSquared(glitchAmount, 0, 2, 0, 2);
-    phaseVocoder.glitchAmount = glitchAmount;
-    phaseVocoder.glitchIntensity = glitchIntensity;
+
 
     
     // CHROMAGRAM!
@@ -396,7 +410,6 @@ void ofApp::audioOut(float* buffer, int bufferSize, int nChannels) {
 //                phaseVocoder.setRandomMode();
             }
         }
-//        setPhaseVocoderMode();
         
 //        sample = pianoSamp.play();
 //        float sample = osc.sinewave(frequency);
@@ -494,10 +507,10 @@ void ofApp::keyReleased(int key) {
 void ofApp::mouseMoved(int x, int y) {
 //    frequency = ofMap(x, 0, ofGetWidth(), 10, 22000);
 
-    float pitchShiftFactor = ofMap(x, 0, ofGetWidth(), -12, 12);
-    float pitchShift = powf(2.0, pitchShiftFactor / 12.0);
-    phaseVocoder.setPitchShift(pitchShift);
-//
+//    float pitchShiftFactor = ofMap(x, 0, ofGetWidth(), -12, 12);
+//    float pitchShift = powf(2.0, pitchShiftFactor / 12.0);
+//    phaseVocoder.setPitchShift(pitchShift);
+////
 //    int pitchCount = (int) ofMap(x, 0, ofGetWidth(), 1, 20);
 //    phaseVocoder.pitchCount = pitchCount;
     
@@ -527,13 +540,13 @@ void ofApp::setGlitchAmount(float _glitchAmount) {
 }
 
 void ofApp::setPhaseVocoderMode() {
-    if (glitchAmount < 0.5) {
-        float pitchShiftDegree = floor(ofMap(glitchAmount, 0, 0.5, 0, 6));
-//        if (ofRandom(1.0) > 0.5) pitchShiftDegree*= -1;
+    if (glitchAmount < 0.25) {
+        float pitchShiftDegree = floor(ofMap(glitchAmount, 0, 0.25, 0, 12));
+        if (ofRandom(1.0) > 0.5) pitchShiftDegree*= -1;
         float pitchShfit = powf(2.0, (int)pitchShiftDegree / 12.0);
-       
-//        phaseVocoder.setPitchShift(pitchShfit);
-//        phaseVocoder.setMode(simplePitchShift);
+//
+        phaseVocoder.setPitchShift(pitchShfit);
+        phaseVocoder.setMode(simplePitchShift);
     } else if (glitchAmount < 0.5) {
 //        phaseVocoder.setMode(multiPitchShift);
     }
@@ -562,7 +575,7 @@ void ofApp::cleanRecording() {
     cout << "Begin cleaned recording" << endl;
     
     // amount to shave off start/end
-    int cleanFromStart = (float)sampleRate / 10.0;
+    int cleanFromStart = (float)sampleRate / 20.0;
     int cleanFromEnd = (float)sampleRate / 20.0;
    
     // get avg sample value
